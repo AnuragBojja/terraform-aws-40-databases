9085b76ac7b78ff2b3e863397cbb0f9eee4204ea
AnuragBojja <anuragbojja19@gmail.com>
2026-04-13 21:04:44 -0500

    40-database

--- locals.tf
+++ locals.tf
@@ -7,7 +7,7 @@ locals {
   common_name = ("${var.project_name}-${var.env}")
   vpc_id = data.aws_ssm_parameter.vpc_id.value
   ami_id = data.aws_ami.roboshop_ami.id
-  database_subnet_ids = data.aws_ssm_parameter.database_subnet_ids.value[0]
+  database_subnet_ids = split(",",data.aws_ssm_parameter.database_subnet_ids.value)[0]
   mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
 
 }
