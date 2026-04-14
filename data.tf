534dd9cddae1acc2773d88468f6749e184234a7c
AnuragBojja <anuragbojja19@gmail.com>
2026-04-13 22:44:01 -0500

    40-database

--- data.tf
+++ data.tf
@@ -21,9 +21,44 @@ data "aws_ami" "roboshop_ami" {
 data "aws_ssm_parameter" "vpc_id" {
   name = "/${var.project_name}/${var.env}/vpc_id"
 }
+#### DATABASES ###
 data "aws_ssm_parameter" "mongodb_sg_id" {
   name = "/${var.project_name}/${var.env}/mongodb_sg_id"
 }
+data "aws_ssm_parameter" "redis_sg_id" {
+  name = "/${var.project_name}/${var.env}/redis_sg_id"
+}
+data "aws_ssm_parameter" "rabbitmq_sg_id" {
+  name = "/${var.project_name}/${var.env}/rabbitmq_sg_id"
+}
+data "aws_ssm_parameter" "mysql_sg_id" {
+  name = "/${var.project_name}/${var.env}/mysql_sg_id"
+}
+
+#### BACKEND ####
+data "aws_ssm_parameter" "cart_sg_id" {
+  name = "/${var.project_name}/${var.env}/cart_sg_id"
+}
+data "aws_ssm_parameter" "catalogue_sg_id" {
+  name = "/${var.project_name}/${var.env}/catalogue_sg_id"
+}
+data "aws_ssm_parameter" "user_sg_id" {
+  name = "/${var.project_name}/${var.env}/user_sg_id"
+}
+data "aws_ssm_parameter" "payment_sg_id" {
+  name = "/${var.project_name}/${var.env}/payment_sg_id"
+}
+data "aws_ssm_parameter" "shipping_sg_id" {
+  name = "/${var.project_name}/${var.env}/shipping_sg_id"
+}
+
+#### SUBNETS ####
 data "aws_ssm_parameter" "database_subnet_ids" {
   name = "/${var.project_name}/${var.env}/database_subnet_ids"
+}
+data "aws_ssm_parameter" "frontend_subnet_ids" {
+  name = "/${var.project_name}/${var.env}/frontend_subnet_ids"
+}
+data "aws_ssm_parameter" "backend_subnet_ids" {
+  name = "/${var.project_name}/${var.env}/backend_subnet_ids"
 }
--- locals.tf
+++ locals.tf
@@ -8,6 +8,18 @@ locals {
   vpc_id = data.aws_ssm_parameter.vpc_id.value
   ami_id = data.aws_ami.roboshop_ami.id
   database_subnet_ids = split(",",data.aws_ssm_parameter.database_subnet_ids.value)[0]
+  
+  ### DATABASES
   mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+  rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
+  redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
+  mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
+  #### BACKEND ###
+  cart_sg_id = data.aws_ssm_parameter.cart_sg_id.value
+  catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
+  user_sg_id = data.aws_ssm_parameter.user_sg_id.value
+  shipping_sg_id = data.aws_ssm_parameter.shipping_sg_id.value
+  payment_sg_id = data.aws_ssm_parameter.payment_sg_id.value
+  
 
 }
